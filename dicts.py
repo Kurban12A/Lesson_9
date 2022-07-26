9419a84cbef586592139355a7943db4df3f7369a
Kurban12A <108360687+Kurban12A@users.noreply.github.com>
2022-07-26 20:49:07 +0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -4,3 +4,10 @@ lst_2 = ['1', '2', '3']
 res = dict(zip(lst_1, lst_2))
 print(res)
 
+
+# 2 
+lst_1 = [1, 2, 3]
+lst_2 = ['1', '2', '3']
+lst_3 = ['A', 'B', 'C']
+dicts = dict(zip(lst_1, zip(lst_2,lst_3)))
+print(dicts)
